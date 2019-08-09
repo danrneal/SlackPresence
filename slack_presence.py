69b238f5a517f48666e631e4e5d217e5f10f88e3
Dan Neal <dan.r.neal@gmail.com>
2019-08-09 13:08:52 -0700

    test for systemd

--- slack_presence.py
+++ slack_presence.py
@@ -18,46 +18,52 @@ def set_slack_presence(presence):
         response.raise_for_status()
 
 
-schedule.every().monday.at(START_TIME).do(
-    set_slack_presence,
-    presence='auto'
-)
-schedule.every().monday.at(QUITTING_TIME).do(
-    set_slack_presence,
-    presence='away'
-)
-schedule.every().tuesday.at(START_TIME).do(
-    set_slack_presence,
-    presence='auto'
-)
-schedule.every().tuesday.at(QUITTING_TIME).do(
-    set_slack_presence,
-    presence='away'
-)
-schedule.every().wednesday.at(START_TIME).do(
-    set_slack_presence,
-    presence='auto'
-)
-schedule.every().wednesday.at(QUITTING_TIME).do(
-    set_slack_presence,
-    presence='away'
-)
-schedule.every().thursday.at(START_TIME).do(
-    set_slack_presence,
-    presence='auto'
-)
-schedule.every().thursday.at(QUITTING_TIME).do(
-    set_slack_presence,
-    presence='away'
-)
-schedule.every().friday.at(START_TIME).do(
-    set_slack_presence,
-    presence='auto'
-)
-schedule.every().friday.at(QUITTING_TIME).do(
-    set_slack_presence,
-    presence='away'
-)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+def scheduler():
+    schedule.every().monday.at(START_TIME).do(
+        set_slack_presence,
+        presence='auto'
+    )
+    schedule.every().monday.at(QUITTING_TIME).do(
+        set_slack_presence,
+        presence='away'
+    )
+    schedule.every().tuesday.at(START_TIME).do(
+        set_slack_presence,
+        presence='auto'
+    )
+    schedule.every().tuesday.at(QUITTING_TIME).do(
+        set_slack_presence,
+        presence='away'
+    )
+    schedule.every().wednesday.at(START_TIME).do(
+        set_slack_presence,
+        presence='auto'
+    )
+    schedule.every().wednesday.at(QUITTING_TIME).do(
+        set_slack_presence,
+        presence='away'
+    )
+    schedule.every().thursday.at(START_TIME).do(
+        set_slack_presence,
+        presence='auto'
+    )
+    schedule.every().thursday.at(QUITTING_TIME).do(
+        set_slack_presence,
+        presence='away'
+    )
+    schedule.every().friday.at(START_TIME).do(
+        set_slack_presence,
+        presence='auto'
+    )
+    schedule.every().friday.at(QUITTING_TIME).do(
+        set_slack_presence,
+        presence='away'
+    )
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    set_slack_presence('away')
+    scheduler()
